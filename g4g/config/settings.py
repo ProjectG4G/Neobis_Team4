753916b2c12c811f7386426afb55f3e41dad9632
Saparmurat <70276825+saparmuratxo@users.noreply.github.com>
2023-02-01 13:12:58 +0600

    Revert "add User app"

--- g4g/config/settings.py
+++ g4g/config/settings.py
@@ -40,10 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'rest_framework'
-    'rest_framework_simplejwt'
-    'users'
 ]
 
 MIDDLEWARE = [
