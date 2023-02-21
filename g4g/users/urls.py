00a874dc5c6f7aa98fe291b0a36f62a1e964022b
Mairam <anarbaevamayram@gmail.com>
2023-02-21 15:18:38 +0600

    Add urls.py

---
+++ g4g/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import RegisterView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='auth_register'),
+]
