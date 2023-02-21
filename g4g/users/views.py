4a34f4d1998ce1a0a1d73dcedd3dd01d475d4a44
Mairam <anarbaevamayram@gmail.com>
2023-02-21 15:18:51 +0600

    Add views.py

---
+++ g4g/users/views.py
@@ -0,0 +1,11 @@
+from .models import User
+from rest_framework.permissions import AllowAny
+
+from .serializers import RegisterSerializer
+from rest_framework import generics
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
