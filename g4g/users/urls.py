2008fffd78ca331a5eb94c7599764bdbb4405444
Saparmurat <saparmurat@protonmail.com>
2023-02-23 16:21:36 +0600

    add logout

--- g4g/users/urls.py
+++ g4g/users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import RegisterView, LoginView
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
-
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView, TokenBlacklistView
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login-phone/', LoginView.as_view(), name='token_obtain_pair_phone'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', TokenBlacklistView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='auth_register'),
 ]
