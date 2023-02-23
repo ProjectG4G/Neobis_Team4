ce867eda9cb57831a8c1fc43efcd285809a7e351
Saparmurat <saparmurat@protonmail.com>
2023-02-23 17:39:54 +0600

    add verification urls

--- g4g/users/urls.py
+++ g4g/users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import RegisterView, LoginView
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView, TokenBlacklistView
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenObtainPairView,
+    TokenBlacklistView,
+)
+
+from .views import RegisterView, LoginView, RequestEmailVerificationView, EmailVerificationView
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -8,4 +14,6 @@ urlpatterns = [
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', TokenBlacklistView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='auth_register'),
+    path('request-verification/', RequestEmailVerificationView.as_view(), name='request_verification'),
+    path('verification/<str:token>/', EmailVerificationView.as_view(), name='verification')
 ]
