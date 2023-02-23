76cf61e364ef5054266bb9df952dbb2f7dedf146
Saparmurat <saparmurat@protonmail.com>
2023-02-23 15:07:34 +0600

    password reset urls

--- g4g/users/urls.py
+++ g4g/users/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
-from .views import RegisterView, LoginView
+from django.urls import path, include
+
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
+from .views import RegisterView, LoginView
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login-phone/', LoginView.as_view(), name='token_obtain_pair_phone'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
+    path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
