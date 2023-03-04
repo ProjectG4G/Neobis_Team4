c322399ecf16e7695183aa5aaa64311b14544d6c
Saparmurat <saparmurat@protonmail.com>
2023-03-04 17:00:05 +0600

    simplejwt authentication

--- g4g/config/settings.py
+++ g4g/config/settings.py
@@ -151,9 +151,12 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny'
-    ],
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
