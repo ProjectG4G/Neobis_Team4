85f084a15f15b61a4ebc52de071e81c5ea384403
Saparmurat <saparmurat@protonmail.com>
2023-03-04 18:52:33 +0600

    fix rest_framework permissions

--- g4g/config/settings.py
+++ g4g/config/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
