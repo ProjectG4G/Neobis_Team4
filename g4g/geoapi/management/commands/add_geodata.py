b8704e7ff1fd028daa91d063aacb7c0eab1eab67
Saparmurat <saparmurat@protonmail.com>
2023-03-08 15:40:51 +0600

    use with clause for safety

--- g4g/geoapi/management/commands/add_geodata.py
+++ g4g/geoapi/management/commands/add_geodata.py
@@ -17,54 +17,56 @@ def does_exist(model, name):
 class Command(BaseCommand):
     help = 'add geodata to databases'
 
-    def add_regions(self):
+    @staticmethod
+    def add_regions():
         csv_path = os.path.join(os.path.dirname(__file__), 'region_district.csv')
-        file = open(csv_path)
-        csv_reader = csv.DictReader(file)
+        with open(csv_path) as file:
+            csv_reader = csv.DictReader(file)
 
-        for row in csv_reader:
-            region = row['region']
-            district = row['district']
+            for row in csv_reader:
+                region = row['region']
+                district = row['district']
 
-            district_name = extract(district)
+                district_name = extract(district)
 
-            if not does_exist(Region, region):
-                region = Region.objects.create(country_id=1, name=region)
-            else:
-                region = Region.objects.get(name=region)
-
-            if 'шаары' in district:
-                if not does_exist(City, district_name):
-                    City.objects.create(name=district_name, region=region)
-            else:
-                if not does_exist(District, district_name):
-                    District.objects.create(name=district_name, region=region)
-
-    def add_districts(self):
-        csv_path = os.path.join(os.path.dirname(__file__), 'district_village.csv')
-        file = open(csv_path)
-        csv_reader = csv.DictReader(file)
-
-        for row in csv_reader:
-            district = row['district']
-            village = row['village']
-
-            district_name = extract(district)
-
-            if 'шаары' in district:
-                if does_exist(City, district_name):
-                    city = City.objects.get(name=district_name)
-                    Village.objects.create(name=village, city=city)
-            else:
-
-                if 'шаары' in village:
-                    if not does_exist(City, extract(village)):
-                        district = District.objects.get(name=district_name)
-                        City.objects.create(name=extract(village), district=district, region=district.region)
+                if not does_exist(Region, region):
+                    region = Region.objects.create(country_id=1, name=region)
                 else:
-                    district = District.objects.get(name=district_name)
-                    if not does_exist(Village, village):
-                        Village.objects.create(name=village, district=district)
+                    region = Region.objects.get(name=region)
+
+                if 'шаары' in district:
+                    if not does_exist(City, district_name):
+                        City.objects.create(name=district_name, region=region)
+                else:
+                    if not does_exist(District, district_name):
+                        District.objects.create(name=district_name, region=region)
+
+    @staticmethod
+    def add_districts():
+        csv_path = os.path.join(os.path.dirname(__file__), 'district_village.csv')
+        with open(csv_path) as file:
+            csv_reader = csv.DictReader(file)
+
+            for row in csv_reader:
+                district = row['district']
+                village = row['village']
+
+                district_name = extract(district)
+
+                if 'шаары' in district:
+                    if does_exist(City, district_name):
+                        city = City.objects.get(name=district_name)
+                        if not Village.objects.filter(name=village, city=city).exists():
+                            Village.objects.create(name=village, city=city)
+                else:
+                    if 'шаары' in village:
+                        if not does_exist(City, extract(village)):
+                            district = District.objects.get(name=district_name)
+                            City.objects.create(name=extract(village), district=district, region=district.region)
+                    else:
+                        district = District.objects.get(name=district_name)
+                        if not Village.objects.filter(name=village, district=district).exists():
+                            Village.objects.create(name=village, district=district)
 
     def handle(self, *args, **options):
         if not does_exist(Country, name='Кыргызстан'):
