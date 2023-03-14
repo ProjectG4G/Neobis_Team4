802eed759b3fc52433540472c170107e68edca3e
Saparmurat <saparmurat@protonmail.com>
2023-03-14 22:59:06 +0600

    admin

--- g4g/geoapi/admin.py
+++ g4g/geoapi/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Country, Region, District, Village, City
+from .models import Country, Region, District, Village
 
 
 # Register your models here.
@@ -9,32 +9,28 @@ class VillageAdmin(admin.ModelAdmin):
     search_fields = ['name__icontains']
     list_display = ('name', 'get_district_name', 'get_city_name', 'get_region_name')
 
-    list_filter = ('district__region', 'city', 'district',)
+    list_filter = ('district__region', 'district',)
 
     @admin.display(description="Район", ordering="district__region_name")
     def get_district_name(self, obj):
-        if obj.district:
+        if obj.district and obj.district.type == 1:
             return obj.district.name
         else:
             return "жок"
 
-    @admin.display(description="Шаар", ordering="city__region__name")
+    @admin.display(description="Шаар", ordering="district__region__name")
     def get_city_name(self, obj):
-        if obj.city:
-            return obj.city.name
+        if obj.district and obj.district.type == 2:
+            return obj.district.name
         else:
             return "жок"
 
     @admin.display(description="Облус", ordering="region__name")
     def get_region_name(self, obj):
-        if obj.city:
-            return obj.city.region.name
-        else:
-            return obj.district.region.name
+        return obj.district.region.name
 
 
 admin.site.register(Country)
 admin.site.register(Region)
-admin.site.register(City)
 admin.site.register(District)
 admin.site.register(Village, VillageAdmin)
