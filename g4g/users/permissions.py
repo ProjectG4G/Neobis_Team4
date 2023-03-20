8fb6800fbecdca1d8a5a464c876ad037faf8dd8f
Saparmurat <saparmurat@protonmail.com>
2023-03-20 16:04:58 +0600

    permissions

--- g4g/users/permissions.py
+++ g4g/users/permissions.py
@@ -8,7 +8,7 @@ class IsProfileOwnerOrAdmin(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
+        if request.user.is_authenticated and request.user.is_staff:
             return True
 
     def has_object_permission(self, request, view, obj):
