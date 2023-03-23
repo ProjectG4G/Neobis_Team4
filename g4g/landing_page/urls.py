2cbb6c72a266a7a6c4002026d193700e81296ac6
n-imashov <nurga1997@gmail.com>
2023-03-23 17:34:46 +0600

    add 'stats' url

--- g4g/landing_page/urls.py
+++ g4g/landing_page/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-from .views import LandingPageViewSet
+from .views import LandingPageViewSet, LandingPageStatisticView
 
 router = SimpleRouter()
 router.register('landing_page', LandingPageViewSet, basename='landing_page')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('stats/', LandingPageStatisticView.as_view(), name='landing_page_stats')
+
 ]
