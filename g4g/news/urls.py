854864dc028c023348875fcb81615ed3f4f7dd91
Saparmurat <saparmurat@protonmail.com>
2023-03-29 21:54:09 +0600

    add images url

--- g4g/news/urls.py
+++ g4g/news/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 
 from rest_framework.routers import SimpleRouter
 
-from .views import TagViewSet, ArticleViewSet
+from .views import TagViewSet, ArticleViewSet, ArticleImageViewSet
 
 router = SimpleRouter()
-router.register('tags', TagViewSet, basename='tag')
+router.register('tags', TagViewSet, basename='article-tag')
 router.register('articles', ArticleViewSet, basename='article')
+router.register('images', ArticleImageViewSet, basename='article-image')
 
 urlpatterns = [
     path('', include(router.urls)),
