be20db3dd7d7cca070ccca9212c593f08496a70d
Saparmurat <saparmurat@protonmail.com>
2023-03-29 21:54:20 +0600

    views

--- g4g/news/views.py
+++ g4g/news/views.py
@@ -1,46 +1,31 @@
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.generics import CreateAPIView, ListAPIView
-from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from .serializers import ArticleSerializer, TagSerializer, ArticleAddTagsSerializer
-from .models import Article, Tag
+from .serializers import (
+    ArticleSerializer,
+    TagSerializer,
+    ArticleAddTagsSerializer,
+    ArticleImageSerializer,
+)
+from .models import Article, Tag, ArticleImage
 from .permissions import IsAdminOrReadOnly
 
 
-# Create your views here.
-
-class TagViewSet(ModelViewSet):
+class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
-class ArticleViewSet(ModelViewSet):
+class ArticleImageViewSet(viewsets.ModelViewSet):
+    queryset = ArticleImage.objects.all()
+    serializer_class = ArticleImageSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     permission_classes = [IsAdminOrReadOnly]
-
-    def get_serializer_class(self):
-        if self.action == 'add_tags':
-            return ArticleAddTagsSerializer
-        return ArticleSerializer
-
-    @action(detail=True, methods=['put'])
-    def add_tags(self, request, pk=None):
-        serializer = ArticleAddTagsSerializer(data=request.data)
-
-        if serializer.is_valid():
-            article = Article.objects.get(pk=pk)
-            serializer.update(instance=article, validated_data=serializer.validated_data)
-            return Response({
-                'detail': 'tags were added',
-                'data': serializer.data,
-            })
-        else:
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
-
-    @action(detail=True, methods=['get'])
-    def kg(self, request):
-        serializer =
