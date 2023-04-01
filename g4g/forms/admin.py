e721b225d9690ea9607b2be611688f28084c53d6
Saparmurat <saparmurat@protonmail.com>
2023-04-01 16:53:26 +0600

    add models to admin

--- g4g/forms/admin.py
+++ g4g/forms/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from parler.admin import TranslatableAdmin
 
-# Register your models here.
+from .models import Application, Question, Response, Form, FormBase
+
+admin.site.register(FormBase)
+admin.site.register(Form, TranslatableAdmin)
+admin.site.register(Question)
+admin.site.register(Application)
+admin.site.register(Response)
