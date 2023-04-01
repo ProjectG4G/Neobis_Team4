f1ffa2484913ede4d1ff9b5ab87750d1f5ea2eb6
Saparmurat <saparmurat@protonmail.com>
2023-04-01 16:00:24 +0600

    add models: FormBase, Form, Question

--- g4g/forms/models.py
+++ g4g/forms/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from parler.models import TranslatableModel, TranslatedFields
 
-# Create your models here.
+
+class FormBase(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+
+class Form(TranslatableModel):
+    translations = TranslatedFields(
+        title=models.TextField(blank=True, default=""),
+        description=models.TextField(blank=True, default=""),
+    )
+
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    active = models.BooleanField(default=False)
+
+    formbase = models.ForeignKey(FormBase, on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    order = models.IntegerField(default=0)
+
+    title = (models.TextField(blank=True, default=""),)
+    description = (models.TextField(blank=True, default=""),)
+
+    content = models.JSONField(
+        blank=True,
+        null=True,
+    )
+
+    required = models.BooleanField(default=False)
+
+    form = models.ForeignKey(Form, on_delete=models.CASCADE)
