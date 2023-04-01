89df90660bdc5162cb711417c3c517b14705903b
Saparmurat <saparmurat@protonmail.com>
2023-04-01 12:55:56 +0600

    add models to admin

--- g4g/news/admin.py
+++ g4g/news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from parler.admin import TranslatableAdmin
+
+from .models import ArticleImage, Article, Tag
+
+admin.site.register(Article, TranslatableAdmin)
+admin.site.register(Tag)
+admin.site.register(ArticleImage)
