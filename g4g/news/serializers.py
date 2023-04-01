d227c687028269e677d6206ce44db453fa189907
Saparmurat <saparmurat@protonmail.com>
2023-04-01 12:58:13 +0600

    add list, create, update serializers

--- g4g/news/serializers.py
+++ g4g/news/serializers.py
@@ -2,90 +2,171 @@ from rest_framework import serializers
 
 from .models import Tag, Article, ArticleImage
 
+from parler_rest.serializers import TranslatableModelSerializer, TranslatedFieldsField
 
-# from parler_rest.serializers import TranslatableModelSerializer, TranslatedFieldsField
+
+def get_language(self):
+    return self.context["request"].headers.get("Accept-Language")
+
+
+def switch_language(lang):
+    if lang == "ky":
+        return "ru"
+    elif lang == "ru":
+        return "ky"
+
+
+def upload_images(uploaded_images, article):
+    for image in uploaded_images:
+        ArticleImage.objects.create(image=image, article=article)
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ['id', 'name']
+        fields = ["id", "name"]
 
 
 class ArticleImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ArticleImage
         fields = [
-            'url',
-            'id',
-            'image',
-            'article',
+            "url",
+            "id",
+            "image",
+            "article",
         ]
 
 
-class ArticleSerializer(serializers.ModelSerializer):
-    tags = TagSerializer(many=True, read_only=True)
-    added_tags = serializers.ListField(
-        child=serializers.IntegerField(),
-        write_only=True,
-        required=False,
-    )
+class ArticleReadOnlySerializer(serializers.ModelSerializer):
+    title = serializers.SerializerMethodField()
+    content = serializers.SerializerMethodField()
+    lang = serializers.SerializerMethodField()
 
     images = ArticleImageSerializer(many=True, read_only=True)
-    uploaded_images = serializers.ListField(
-        child=serializers.ImageField(allow_empty_file=False, use_url=False),
-        write_only=True,
-        required=False
-    )
-
-    title = serializers.SerializerMethodField('get_title')
-    content = serializers.SerializerMethodField('get_title')
+    tags = TagSerializer(many=True, read_only=True)
 
     class Meta:
         model = Article
-        fields = [
-            'id',
-            'section',
-            'images',
-            'uploaded_images',
-            'added_tags',
-            'published',
-            'edited',
-            'tags',
-        ]
+        fields = (
+            "id",
+            "url",
+            "lang",
+            "title",
+            "content",
+            "images",
+            "tags",
+            "section",
+            "published",
+            "edited",
+        )
+
+    def get_lang(self, obj):
+        return get_language(self)
+
+    def get_title(self, obj):
+        obj.set_current_language(get_language(self))
+        return obj.title
+
+    def get_content(self, obj):
+        obj.set_current_language(get_language(self))
+        return obj.content
+
+
+class ArticleCreateSerializer(serializers.ModelSerializer):
+    uploaded_images = serializers.ListField(
+        child=serializers.ImageField(allow_empty_file=True, required=False),
+        write_only=True,
+        required=False,
+        allow_empty=True,
+    )
+
+    title = serializers.CharField()
+    content = serializers.CharField()
+
+    class Meta:
+        model = Article
+        fields = (
+            "title",
+            "content",
+            "uploaded_images",
+            "section",
+        )
 
     def create(self, validated_data):
-        uploaded_images = validated_data.pop("uploaded_images")
-        added_tags = validated_data.pop("added_tags")
+        language = get_language(self)
 
-        for image in uploaded_images:
-            ArticleImage.objects.create(article=article, image=image)
+        article = Article.objects.language(language).create(**validated_data)
 
-        for tag in added_tags:
-            article.tags.add(Tag.objects.get(id=tag))
+        article.set_current_language(switch_language(language))
+
+        article.title = ""
+        article.content = ""
+
+        article.save()
+
+        upload_images(validated_data.pop("uploaded_images", []), article=article)
+
+        article.set_current_language(language)
 
         return article
 
 
-class ArticleContentSerializer(serializers.ModelSerializer):
-    pass
-
-
-class AddTagSerializer(serializers.Serializer):
+class ArticlePreCreateSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Tag
-        fields = ['id']
+        model = Article
+        fields = ("section",)
+
+    def create(self, validated_data):
+        validated_data["title"] = ""
+        validated_data["content"] = ""
+        article = Article.objects.language("ky").create(**validated_data)
+
+        article.set_current_language("ru")
+
+        article.title = ""
+        article.content = ""
+
+        article.save()
+
+        return article
 
 
-class ArticleAddTagsSerializer(serializers.Serializer):
-    added_tags = serializers.ListField(
-        child=AddTagSerializer(),
+class ArticleUpdateSerializer(serializers.Serializer):
+    title = serializers.CharField(write_only=True, required=True)
+    content = serializers.CharField(write_only=True, required=True)
+
+    uploaded_images = serializers.ListField(
+        child=serializers.ImageField(required=False, allow_empty_file=True),
         write_only=True,
         required=False,
     )
 
     def update(self, instance, validated_data):
-        added_tags = validated_data['added_tags']
-        for tag in added_tags:
-            print(tag)
-            instance.tags.add(Tag.objects.get(id=tag))
+        instance.set_current_language(get_language(self))
+
+        instance.title = validated_data["title"]
+        instance.content = validated_data["content"]
+
+        instance.save()
+
+        upload_images(validated_data.pop("uploaded_images", []), article=instance)
+
+        return instance
+
+
+class ArticleParlerSerializer(TranslatableModelSerializer):
+    images = ArticleImageSerializer(many=True, read_only=True)
+    translations = TranslatedFieldsField(shared_model=Article)
+
+    class Meta:
+        model = Article
+        fields = (
+            "id",
+            "translations",
+            "section",
+            "tags",
+            "images",
+            "published",
+            "edited",
+        )
