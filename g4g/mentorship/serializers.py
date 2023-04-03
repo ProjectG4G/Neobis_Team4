4273d3ba3e43bb852e3f9df2a5b4dedb054c9e1c
Saparmurat <saparmurat@protonmail.com>
2023-04-04 00:46:09 +0600

    add user info

--- g4g/mentorship/serializers.py
+++ g4g/mentorship/serializers.py
@@ -1,9 +1,4 @@
-from rest_framework.serializers import (
-    ModelSerializer,
-    FileField,
-    ImageField,
-    ListField,
-)
+from rest_framework import serializers
 
 from geoapi.models import (
     Region,
@@ -22,17 +17,19 @@ from .models import (
 )
 
 
-class MentorshipImageSerializer(ModelSerializer):
+class MentorshipImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = MentorshipImage
         fields = "__all__"
 
 
-class MentorshipSerializer(ModelSerializer):
+class MentorshipSerializer(serializers.ModelSerializer):
     images = MentorshipImageSerializer(many=True, read_only=True)
 
-    uploaded_images = ListField(
-        child=ImageField(allow_empty_file=True, use_url=False, required=False),
+    uploaded_images = serializers.ListField(
+        child=serializers.ImageField(
+            allow_empty_file=True, use_url=False, required=False
+        ),
         write_only=True,
         required=False,
     )
@@ -40,22 +37,22 @@ class MentorshipSerializer(ModelSerializer):
     class Meta:
         model = Mentorship
         fields = [
-            'id',
-            'created',
-            'edited',
-            'title',
-            'header1',
-            'header2',
-            'body1',
-            'body2',
-            'body3',
-            'images',
-            'uploaded_images',
+            "id",
+            "created",
+            "edited",
+            "title",
+            "header1",
+            "header2",
+            "body1",
+            "body2",
+            "body3",
+            "images",
+            "uploaded_images",
         ]
 
     def create(self, validated_data):
         uploaded_images = None
-        if validated_data.__contains__('uploaded_images'):
+        if validated_data.__contains__("uploaded_images"):
             uploaded_images = validated_data.pop("uploaded_images")
 
         mentorship = Mentorship.objects.create(**validated_data)
@@ -66,71 +63,98 @@ class MentorshipSerializer(ModelSerializer):
         return mentorship
 
 
-class ApplicationsSerializer(ModelSerializer):
+class ApplicationsSerializer(serializers.ModelSerializer):
     class Meta:
         model = MentorshipApplications
-        fields = '__all__'
-        read_only_fields = ('user', 'is_accepted')
+        fields = "__all__"
+        read_only_fields = ("user", "is_accepted")
 
     def validate(self, attrs):
-        region = attrs['region']
-        district = attrs['district']
-        village = attrs['village']
+        region = attrs["region"]
+        district = attrs["district"]
+        village = attrs["village"]
 
         if not Region.objects.filter(id=region.id).exists():
-            raise serializers.ValidationError({'detail': 'Given region {} does not exist!'.format(region)})
+            raise serializers.ValidationError(
+                {"detail": "Given region {} does not exist!".format(region)}
+            )
 
         if not District.objects.filter(id=district.id).exists():
-            raise serializers.ValidationError({'detail': 'Given district/city {} does not exist!'.format(district)})
+            raise serializers.ValidationError(
+                {"detail": "Given district/city {} does not exist!".format(district)}
+            )
 
         if village and not Village.objects.filter(id=village.id).exists():
-            raise serializers.ValidationError({'detail': 'Given village {} does not exist!'.format(village)})
+            raise serializers.ValidationError(
+                {"detail": "Given village {} does not exist!".format(village)}
+            )
 
         if district.region != region:
             raise serializers.ValidationError(
-                {'detail': 'Given district or city {} doesn\'t belong to region {}'.format(district, region)})
+                {
+                    "detail": "Given district or city {} doesn't belong to region {}".format(
+                        district, region
+                    )
+                }
+            )
 
         if village and village.district != district:
             raise serializers.ValidationError(
-                {'detail': 'Given village {} doesn\'t belong to district {}'.format(village, district)})
+                {
+                    "detail": "Given village {} doesn't belong to district {}".format(
+                        village, district
+                    )
+                }
+            )
 
         return attrs
 
     def create(self, validated_data):
         request = self.context.get("request")
 
-        validated_data['user'] = request.user
+        validated_data["user"] = request.user
 
         application = MentorshipApplications.objects.create(**validated_data)
 
         return application
 
 
-class FeedbackSerializer(ModelSerializer):
+class FeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
-        fields = '__all__'
+        fields = "__all__"
 
 
-class FAQSerializer(ModelSerializer):
+class FAQSerializer(serializers.ModelSerializer):
     class Meta:
         model = FAQ
-        fields = '__all__'
+        fields = "__all__"
 
 
-class QuestionSerializer(ModelSerializer):
+class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = MentorshipQuestions
-        fields = '__all__'
+        fields = "__all__"
 
 
-class MentorProfileSerializer(ModelSerializer):
+class MentorProfileSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(source="user.first_name", read_only=True)
+    last_name = serializers.CharField(source="user.last_name", read_only=True)
+
+    profile_picture = serializers.ImageField(
+        source="user.profile_picture", read_only=True
+    )
+
     class Meta:
         model = MentorProfile
         fields = (
-            'id',
-            'user',
-            'image',
-            'description',
+            "id",
+            "user",
+            "url",
+            "first_name",
+            "last_name",
+            "profile_picture",
+            "image",
+            "description",
         )
-        read_only_fields = ('user',)
+        read_only_fields = ("user",)
