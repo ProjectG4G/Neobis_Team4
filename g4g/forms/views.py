55292b3666be732c04a0c34880f3c4a25a064589
Saparmurat <saparmurat@protonmail.com>
2023-04-06 14:01:27 +0600

    distinct serializers

--- g4g/forms/views.py
+++ g4g/forms/views.py
@@ -2,10 +2,18 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
 from .models import Form
-from .serializers import FormParlerSerializer
+from .serializers import (
+    FormParlerSerializer,
+    FormReadOnlySerializer,
+    FormCreateUpdateSerializer,
+)
 
 
 class FormParlerViewSet(viewsets.ModelViewSet):
     queryset = Form.objects.all()
-    serializer_class = FormParlerSerializer
     permission_classes = (AllowAny,)
+
+    def get_serializer_class(self):
+        if self.action in ["get", "list", "retrieve"]:
+            return FormParlerSerializer
+        return FormCreateUpdateSerializer
