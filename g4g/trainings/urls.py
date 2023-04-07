911607a9afddfcdb9f19570c1651dc301b715f68
Mairam <anarbaevamayram@gmail.com>
2023-04-07 12:50:30 +0600

    change urls.py

--- g4g/trainings/urls.py
+++ g4g/trainings/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from .views import TrainingsViewSet, CommentViewSet, RatingViewSet, FAQViewSet, QuestionsViewSet, ApplicationsViewSet
+from .views import TrainingsParlerViewSet
 
 router = SimpleRouter()
-router.register('trainings', TrainingsViewSet, basename='training')
-router.register('questions', QuestionsViewSet, basename='questions')
-router.register('applications', ApplicationsViewSet, basename='applications')
-router.register('comments', CommentViewSet, basename='comment')
-router.register('ratings', RatingViewSet, basename='rating')
-router.register('FAQ', FAQViewSet, basename='FAQ')
-
+router.register("trainings", TrainingsParlerViewSet, basename="training")
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
