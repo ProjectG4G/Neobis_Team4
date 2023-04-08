22b6f1dda59b3621aca27cc150ca3ad164e64adf
Saparmurat <saparmurat@protonmail.com>
2023-04-09 02:34:44 +0600

    views

--- g4g/forms/views.py
+++ g4g/forms/views.py
@@ -1,19 +1,80 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
-from .models import Form
+from drf_spectacular.utils import extend_schema
+
+from .models import (
+    Form,
+    Event,
+    Question,
+    Choice,
+    Response,
+    Application,
+    EventImage,
+)
+
 from .serializers import (
+    EventParlerSerializer,
     FormParlerSerializer,
-    FormReadOnlySerializer,
-    FormCreateUpdateSerializer,
+    QuestionSerializer,
+    QuestionChoiceSerializer,
+    ApplicationSerializer,
+    ApplicationCreateSerializer,
+    ResponseSerializer,
+    EventImageSerializer,
+    ChoiceSerializer,
 )
 
 
+@extend_schema(
+    tags=["Events"],
+    description="Events include Trainings, Mentorship program.",
+)
+class EventParlerViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventParlerSerializer
+
+
+@extend_schema(
+    tags=["Event Images"],
+    description="Images for Events",
+)
+class EventImageViewSet(viewsets.ModelViewSet):
+    queryset = EventImage.objects.all()
+    serializer_class = EventImageSerializer
+
+
+@extend_schema(tags=["Forms"])
 class FormParlerViewSet(viewsets.ModelViewSet):
     queryset = Form.objects.all()
     permission_classes = (AllowAny,)
+    serializer_class = FormParlerSerializer
+
+
+@extend_schema(tags=["Form Questions"], description="Questions in Forms")
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+@extend_schema(tags=["Questions Choices"], description="Choices in Question")
+class ChoiceViewSet(viewsets.ModelViewSet):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+
+@extend_schema(tags=["Applications"], description="Application from users")
+class ApplicationViewSet(viewsets.ModelViewSet):
+    queryset = Application.objects.all()
 
     def get_serializer_class(self):
-        if self.action in ["get", "list", "retrieve"]:
-            return FormParlerSerializer
-        return FormCreateUpdateSerializer
+        if self.action in ["retrieve", "list"]:
+            return ApplicationSerializer
+        else:
+            return ApplicationCreateSerializer
+
+
+@extend_schema(tags=["Applications Responses"])
+class ResponseViewSet(viewsets.ModelViewSet):
+    queryset = Response.objects.all()
+    serializer_class = ResponseSerializer
