2d7e9ce559b0c3d7bf74635a0ea25796213bda3d
Saparmurat <saparmurat@protonmail.com>
2023-04-09 02:25:20 +0600

    urls

--- g4g/mentorship/urls.py
+++ g4g/mentorship/urls.py
@@ -1,20 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from .views import (
-    MentorshipViewSet,
-    ApplicationsViewSet,
-    FeedbackViewSet,
-    FAQViewSet,
-    QuestionsViewSet,
     MentorProfileViewSet,
 )
 
 router = SimpleRouter()
-router.register("programs", MentorshipViewSet, basename="program")
-router.register("questions", QuestionsViewSet, basename="question")
-router.register("applications", ApplicationsViewSet, basename="application")
-router.register("feedback", FeedbackViewSet, basename="feedback")
-router.register("FAQ", FAQViewSet, basename="FAQ")
+
 router.register("mentors", MentorProfileViewSet, basename="mentorprofile")
 
 urlpatterns = [
