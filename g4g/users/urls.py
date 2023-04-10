5dbffcce6b380942d1a2460d657c9e79244c8512
Saparmurat <saparmurat@protonmail.com>
2023-04-10 23:02:15 +0600

    format

--- g4g/users/urls.py
+++ g4g/users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
-    TokenObtainPairView,
     TokenBlacklistView,
 )
 
@@ -19,25 +18,40 @@ from .views import (
     ModeratorViewSet,
     MentorProfileView,
     LoginEmailView,
-    UserProfileRetrieveView
+    UserProfileRetrieveView,
 )
 
 router = SimpleRouter()
-router.register('users', UserProfileView, basename='user')
-router.register('moderators', ModeratorViewSet, basename='moderator')
+router.register("users", UserProfileView, basename="user")
+router.register("moderators", ModeratorViewSet, basename="moderator")
 
 urlpatterns = [
-    path('login/', LoginEmailView.as_view(), name='login_email'),
-    path('login-phone/', LoginPhoneView.as_view(), name='login_phone'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('logout/', TokenBlacklistView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register_email'),
-    path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-    path('change-password/', ChangePasswordView.as_view(), name='change_password'),
-    path('verification/', EmailVerificationView.as_view(), name='verification'),
-    path('verification/confirm/', EmailVerificationConfirmView.as_view(), name='verification_confirm'),
-    path('', include(router.urls)),
-    path('stats/new-users/<int:year>', UserRegisterStatisticView.as_view(), name='stats-new-users'),
-    path('users/<int:pk>/mentor_profile/', MentorProfileView.as_view(), name='mentor_profile'),
-    path('current_user_profile/', UserProfileRetrieveView.as_view(), name='user_profile'),
+    path("login/", LoginEmailView.as_view(), name="login_email"),
+    path("login_phone/", LoginPhoneView.as_view(), name="login_phone"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("logout/", TokenBlacklistView.as_view(), name="logout"),
+    path("register/", RegisterView.as_view(), name="register_email"),
+    path(
+        "password_reset/",
+        include("django_rest_passwordreset.urls", namespace="password_reset"),
+    ),
+    path("change_password/", ChangePasswordView.as_view(), name="change_password"),
+    path("verification/", EmailVerificationView.as_view(), name="verification"),
+    path(
+        "verification/confirm/",
+        EmailVerificationConfirmView.as_view(),
+        name="verification_confirm",
+    ),
+    path("", include(router.urls)),
+    path(
+        "users/<int:pk>/mentor_profile/",
+        MentorProfileView.as_view(),
+        name="mentor_profile",
+    ),
+    path("current_user_profile/", UserProfileRetrieveView.as_view(), name="user_profile"),
+    path(
+        "stats/new-users/<int:year>",
+        UserRegisterStatisticView.as_view(),
+        name="stats-new-users",
+    ),
 ]
