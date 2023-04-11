3a894c47e5bfaa025cb9137cd2e08f568726919a
Saparmurat <saparmurat@protonmail.com>
2023-04-11 23:07:29 +0600

    fix urls

--- g4g/news/urls.py
+++ g4g/news/urls.py
@@ -2,12 +2,16 @@ from django.urls import path, include
 
 from rest_framework.routers import SimpleRouter
 
-from .views import TagViewSet, ArticleViewSet, ArticleImageViewSet
+from .views import (
+    TagViewSet,
+    ArticleImageViewSet,
+    ArticleParlerViewSet,
+)
 
 router = SimpleRouter()
-router.register("tags", TagViewSet, basename="article-tag")
-router.register("articles", ArticleViewSet, basename="article")
-router.register("images", ArticleImageViewSet, basename="article-image")
+router.register("tags", TagViewSet, basename="tag")
+router.register("images", ArticleImageViewSet, basename="articleimage")
+router.register("articles", ArticleParlerViewSet, basename="article")
 
 urlpatterns = [
     path("", include(router.urls)),
