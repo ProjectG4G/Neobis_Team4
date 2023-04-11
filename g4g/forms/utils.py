3c068f1c9f63f5377263ee313404433a52220184
Saparmurat <saparmurat@protonmail.com>
2023-04-11 23:12:15 +0600

    fix arguments, delete unnecessary utils

--- g4g/forms/utils.py
+++ g4g/forms/utils.py
@@ -1,21 +1,6 @@
 from .models import EventImage
 
 
-def get_language(self):
-    lang = self.context["request"].headers.get("Accept-Language")
-
-    if lang == "ru":
-        return "ru"
-    return "ky"
-
-
-def switch_language(lang):
-    if lang == "ky":
-        return "ru"
-    elif lang == "ru":
-        return "ky"
-
-
 def upload_images(images, event):
     for image in images:
-        EventImage.objects.create(image, event=event)
+        EventImage.objects.create(image=image, event=event)
