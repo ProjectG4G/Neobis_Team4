53301b8cf9c350d78dd2ffba7813adcd49b845c9
Saparmurat <saparmurat@protonmail.com>
2023-04-11 23:06:51 +0600

    add Applicants

--- g4g/trainings/models.py
+++ g4g/trainings/models.py
@@ -1,1 +1,6 @@
 from django.db import models
+
+
+class Applicants(models.Model):
+    training = models.ForeignKey("forms.Event", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
