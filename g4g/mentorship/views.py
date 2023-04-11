994fb803fec0c65209a618e46e149e9daf5f9b5f
Saparmurat <saparmurat@protonmail.com>
2023-04-11 14:33:50 +0600

    views

--- g4g/mentorship/views.py
+++ g4g/mentorship/views.py
@@ -1,23 +1,50 @@
 from django_filters.rest_framework import DjangoFilterBackend
 
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from drf_spectacular.utils import extend_schema
 
+from forms.models import Event, Application
+from forms.serializers import EventParlerSerializer, ApplicationSerializer
+from forms.views import ApplicationViewSet
+
 from .serializers import (
     MentorProfileSerializer,
+    MenteeSerializer,
 )
 from .models import (
     MentorProfile,
+    Mentee,
 )
 
 from .permissions import IsAdminOrReadOnly
 
 
+@extend_schema(tags=["Mentorship Programs"])
+class MentorshipViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.filter(type="mentorship")
+    serializer_class = EventParlerSerializer
+
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+@extend_schema(tags=["Mentees"])
+class MenteeViewSet(viewsets.ModelViewSet):
+    queryset = Mentee.objects.all()
+    serializer_class = MenteeSerializer
+
+    permission_classes = (IsAdminOrReadOnly,)
+
+
 @extend_schema(tags=["Mentor Profiles"])
-class MentorProfileViewSet(ModelViewSet):
+class MentorProfileViewSet(viewsets.ModelViewSet):
     queryset = MentorProfile.objects.all()
     serializer_class = MentorProfileSerializer
+
     permission_classes = [IsAdminOrReadOnly]
 
-    allowed_methods = ["GET", "PUT", "PATCH", "HEAD", "OPTIONS"]
+
+@extend_schema(tags=["Mentorship Applications"])
+class MentorshipApplicationsViewSet(ApplicationViewSet):
+    queryset = Application.objects.filter(form__event__type="mentorship")
