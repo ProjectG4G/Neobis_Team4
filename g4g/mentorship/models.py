63f7998a3328b80fe7b8993c579c9e4da83f8c04
Saparmurat <saparmurat@protonmail.com>
2023-04-11 14:33:26 +0600

    add Mentee

--- g4g/mentorship/models.py
+++ g4g/mentorship/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 
 
-class MentorProfile(models.Model):
+class Mentee(models.Model):
+    objects = models.Manager()
+
+    program = models.ForeignKey("forms.Event", on_delete=models.CASCADE)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+
+class MentorProfile(models.Model):
+    objects = models.Manager()
+
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="mentor"
+    )
     image = models.ImageField(upload_to="images/mentor/", null=True)
     description = models.TextField(null=True)
+    mentees = models.ManyToManyField("Mentee", blank=True)
