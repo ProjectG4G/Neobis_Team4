2eee3c771f115e67a457cf3f0e2ebdb79d24138d
Saparmurat <saparmurat@protonmail.com>
2023-04-11 23:20:20 +0600

    delete unnecessary

--- g4g/landing_page/urls.py
+++ g4g/landing_page/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path, include
-from rest_framework.routers import SimpleRouter
-from .views import LandingPageViewSet, LandingPageStatisticView
+from django.urls import path
+from .views import LandingPageStatisticView
 
-router = SimpleRouter()
-router.register('landing_page', LandingPageViewSet, basename='landing_page')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('landing_page/stats/', LandingPageStatisticView.as_view(), name='landing_page_stats')
-
+    path(
+        "landing_page/stats/",
+        LandingPageStatisticView.as_view(),
+        name="landing_page_stats",
+    )
 ]
--- g4g/landing_page/views.py
+++ g4g/landing_page/views.py
@@ -1,20 +1,11 @@
-from rest_framework import viewsets
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 
 from users.models import User
 from forms.models import Event
 
-from .models import LandingPage
-from .serializers import LandingPageSerializer
 
-
-class LandingPageViewSet(viewsets.ModelViewSet):
-    serializer_class = LandingPageSerializer
-    queryset = LandingPage.objects.all()
-
-
-class LandingPageStatisticView(APIView):
+class LandingPageStatisticView(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
         region_count = 0
         for i in range(7):
