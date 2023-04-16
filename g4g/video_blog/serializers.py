7ce01d4560188686a689e5dac1b3896db903d0e3
n-imashov <nurga1997@gmail.com>
2023-04-17 00:38:21 +0600

    serializers

--- g4g/video_blog/serializers.py
+++ g4g/video_blog/serializers.py
@@ -1,27 +1,33 @@
 from rest_framework import serializers
-from .models import VideoCategory, Video, Comment
+from .models import Playlist, Video, Comment, RecentlyWatched
 from parler_rest.serializers import TranslatableModelSerializer, TranslatedFieldsField
-
-
-class VideoCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VideoCategory
-        fields = (
-            "id",
-            "name",
-            "description",
-            "image",
-        )
 
 
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
         fields = (
-            'id',
-            'title',
-            'description',
-            'video_url',
+            "id",
+            "url",
+            "playlist",
+            "title",
+            "description",
+            "video_url",
+        )
+
+
+class PlaylistSerializer(TranslatableModelSerializer):
+    translations = TranslatedFieldsField(shared_model=Playlist, required=False)
+    videos = VideoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Playlist
+        fields = (
+            "id",
+            "url",
+            "translations",
+            "image",
+            "videos",
         )
 
 
@@ -29,7 +35,29 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = (
-            'id',
-            'content',
-            'creation_at',
+            "id",
+            "url",
+            "content",
+            "video",
+            "created_at",
         )
+
+
+class RecentlyWatchedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecentlyWatched
+        fields = (
+            "id",
+            "url",
+            "user",
+            "video",
+            "timestamp",
+        )
+        read_only_fields = ("user",)
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        video = validated_data['video']
+        recently_watched = RecentlyWatched.objects.create(user=user, video=video)
+        recently_watched.save()
+        return recently_watched
