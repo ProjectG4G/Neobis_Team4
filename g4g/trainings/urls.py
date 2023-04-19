7e72c1561b1cc2821690bf72425bb67bacd7d509
Saparmurat <saparmurat@protonmail.com>
2023-04-19 18:42:46 +0600

    add applications

--- g4g/trainings/urls.py
+++ g4g/trainings/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 
 from rest_framework.routers import SimpleRouter
 
-from .views import TrainingViewSet
+from .views import TrainingViewSet, TrainingApplicationViewSet
 
 router = SimpleRouter()
 router.register("trainings", TrainingViewSet, basename="training")
+router.register("applications", TrainingApplicationViewSet, basename="applications")
 
 urlpatterns = [
     path("", include(router.urls)),
