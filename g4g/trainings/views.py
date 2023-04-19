167ad700ce85f2a7b1fd8f3cb5eaedc24afc0928
Saparmurat <saparmurat@protonmail.com>
2023-04-19 19:04:11 +0600

    views

--- g4g/trainings/views.py
+++ g4g/trainings/views.py
@@ -1,23 +1,28 @@
 from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework import viewsets, filters
+from rest_framework.decorators import action
 
 from drf_spectacular.utils import extend_schema
 
 from forms.models import Event
-from forms.serializers import EventParlerSerializer
+from forms.views import ApplicationViewSet, EventParlerViewSet
+from forms.models import Application
+
+from .utils import accept_training
 
 from .permissions import IsAdminOrReadOnly
 
 
 @extend_schema(tags=["Trainings"])
-class TrainingViewSet(viewsets.ModelViewSet):
+class TrainingViewSet(EventParlerViewSet):
     queryset = Event.objects.filter(type="training")
-    serializer_class = EventParlerSerializer
 
-    permission_classes = (IsAdminOrReadOnly,)
 
-    filter_fields = (
-        "title",
-        "",
-    )
+@extend_schema(tags=["Training Applications"])
+class TrainingApplicationViewSet(ApplicationViewSet):
+    queryset = Application.objects.filter(form__event__type="training")
+
+    @action(methods=["put"], detail=True)
+    def accept(self, request, pk=None):
+        return accept_training(self, self.get_object())
